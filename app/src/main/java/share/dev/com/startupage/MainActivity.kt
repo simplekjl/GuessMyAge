f025fb45a2b54a4e3e6959ec160ecdf82a73a54e
Jose Luis <jlcs@Joses-MacBook-Pro.local>
2017-11-28 16:37:58 -0800

    GuessMyAge | Logic separated into functions

--- app/src/main/java/share/dev/com/startupage/MainActivity.kt
+++ app/src/main/java/share/dev/com/startupage/MainActivity.kt
@@ -2,6 +2,7 @@ package share.dev.com.startupage
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -10,15 +11,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //no need to ask for the view from id kotlin has already implemented the classes to do this
-        btnGetMyAge.setOnClickListener{
-            //fire when button is clicked
-            //values coming from textviews have to be vonverted into Strings frist to be able to play with them after
-            var yearofBirth : Int = etDOB.text.toString().toInt()
-            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
-            val myAge : Int = currentYear - yearofBirth
-            tvAge.text = "Your age is $myAge years"
+//        //no need to ask for the view from id kotlin has already implemented the classes to do this
+//        btnGetMyAge.setOnClickListener{
+//            //fire when button is clicked
+//            //values coming from textviews have to be vonverted into Strings frist to be able to play with them after
+//            var yearofBirth : Int = etDOB.text.toString().toInt()
+//            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+//            val myAge : Int = currentYear - yearofBirth
+//            tvAge.text = "Your age is $myAge years"
+//
+//        }
+    }
 
-        }
+    fun getMyAge(view : View){
+        //fire when button is clicked
+        //values coming from textviews have to be vonverted into Strings frist to be able to play with them after
+        var yearofBirth : Int = etDOB.text.toString().toInt()
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val myAge : Int = currentYear - yearofBirth
+        tvAge.text = "Your age is $myAge years"
     }
 }
